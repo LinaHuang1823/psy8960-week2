8efdf035e5c275355c084d5d5b576b9cd83ec02c
Lina Huang <huan2384@umn.edu>
2023-01-27 11:47:23 -0600

    histogram & list

--- R/week2.R
+++ R/week2.R
@@ -6,3 +6,5 @@ rt_df$gender<-factor(rt_df$gender, levels=c("M","F","N"),labels=c("Male", "Femal
 #Analysis
 mean(rt_df$rt)
 rt_f_df<-data.frame(subset(rt_df, subset = gender=="Female"))
+hist(rt_f_df$rt)
+datasets<-list(rt_df,rt_f_df)
