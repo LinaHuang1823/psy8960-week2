a8cce939d1b16917d88168d50a527c7a33b1b558
Lina Huang <huan2384@umn.edu>
2023-01-27 11:24:41 -0600

    mean & data frame

--- R/week2.R
+++ R/week2.R
@@ -2,3 +2,7 @@
 rt_df<-read_csv("data/week2.csv", header = TRUE)
 rt_df$condition<-factor(rt_df$condition, levels=c("A","B"),labels=c("Control", "Experimental"))
 rt_df$gender<-factor(rt_df$gender, levels=c("M","F","N"),labels=c("Male", "Female","Nonbinary"))
+
+#Analysis
+mean(rt_df$rt)
+rt_f_df<-data.frame(subset(rt_df, subset = gender=="Female"))
